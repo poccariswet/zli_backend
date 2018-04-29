6d56b70e044b1406dd91b7a7003e553557bc2144
(Daiki Ogawa) <desighnium.ogawa@gmail.com>
2018-04-29 10:53:38 +0900

    add: eslint

--- js/test.js
+++ js/test.js
@@ -1,15 +1,20 @@
-const { Pool, Client } = require('pg');
+const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.USER,
-    host: process.env.HOST,
-    database: process.env.DATABASE,
-    password: process.env.PASSWORD,
-    port: process.env.PORT,
-})
+	user: process.env.USER,
+	host: process.env.HOST,
+	database: process.env.DATABASE,
+	password: process.env.PASSWORD,
+	port: process.env.PORT,
+});
 
 pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-})
+	console.log(err, res);
+	pool.end();
+});
+
+console.log(
+	'good',
+	'good'
+);
